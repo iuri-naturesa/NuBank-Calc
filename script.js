4d5dec9aff6c9d61ebb1ad412c8f0ac113d4180a
Iuri-Naturesa <98425830+Iuri-Naturesa@users.noreply.github.com>
2022-12-20 18:42:48 -0300

    refatoração do projeto

--- script.js
+++ script.js
@@ -1,26 +1,68 @@
-function calcular() {
-  var valorTotal = document.querySelector("#valortotal").value;
-  var parcelasTotais = document.querySelector("#parcelastotais").value;
-  var parcelasPagas = document.querySelector("#parcelaspagas").value;
-  var adiantamento = document.querySelector("#adiantamento").value;
+const msg = document.querySelectorAll(".msg-chat");
+const texto = document.querySelectorAll(".msg-texto");
+const dados = [];
+const enviar = document.querySelector("#enviar");
 
-  var valorTotalN = Number(valorTotal);
-  var parcelasTotaisN = Number(parcelasTotais);
-  var parcelasPagasN = Number(parcelasPagas);
-  var adiantamentoN = Number(adiantamento);
+var valorTotal;
+var parcelasTotais;
+var parcelasPagas;
+var adiantamento;
+
+document
+  .getElementById("valor-chat")
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.key === "Enter") {
+      document.getElementById("enviar").click();
+    }
+  });
+
+enviar.addEventListener("click", () => {
+  msgEnviada(index);
+  enviarMsg("enviar");
+  msg[index].scrollIntoView();
+  updateDados();
+  document.getElementById("valor-chat").value = "";
+});
+let index = 0;
+
+function msgEnviada(index) {
+  msg[index].classList.add("ativo");
+}
+function enviarMsg(operacao) {
+  if (operacao === "enviar") {
+    msgEnviada((index += 1));
+    msgEnviada((index += 1));
+  }
+}
+function updateDados() {
+  if (document.getElementById("valor-chat").value == "") {
+    dados.push(0);
+  } else {
+    dados.push(document.getElementById("valor-chat").value);
+  }
+  valorTotal = Number(dados[0]);
+  parcelasTotais = Number(dados[1]);
+  parcelasPagas = Number(dados[2]);
+  adiantamento = Number(dados[3]);
 
   const taxa = 0.816 / 100;
-  var valorParcelaN = valorTotalN / parcelasTotaisN;
-  var parcelaFinalN = parcelasTotaisN - adiantamentoN + 1;
-  var somaParcelasN = (parcelasTotaisN + parcelaFinalN) * (adiantamentoN / 2);
+  var valorParcela = valorTotal / parcelasTotais;
+  var parcelasAdiantadas = parcelasTotais - parcelasPagas;
+  var parcelaFinal = parcelasAdiantadas - adiantamento + 1;
+  var somaParcelas = (parcelasTotais + parcelaFinal) * (adiantamento / 2);
 
-  var economia = valorParcelaN * somaParcelasN * taxa;
+  var precoJuntos = valorParcela * adiantamento;
+  var economia = valorParcela * somaParcelas * taxa;
+  var precoEconomia = precoJuntos - economia;
 
-  document.querySelector("#resultado").innerHTML = economia + " " + "Reais";
-
-  console.log(parcelasTotaisN);
-  console.log(parcelaFinalN);
-  console.log(valorParcelaN);
-  console.log(somaParcelasN);
-  console.log(economia);
+  texto[1].innerHTML = `R$${valorTotal}`;
+  texto[3].innerHTML = `Parcelei em ${parcelasTotais} vezes.`;
+  texto[5].innerHTML = `Paguei ${parcelasPagas} parcela(s).`;
+  texto[7].innerHTML = `Desejo adiantar ${adiantamento} parcela(s).`;
+  texto[8].innerHTML = `De R$${precoJuntos.toFixed(
+    2
+  )} por R$${precoEconomia.toFixed(
+    2
+  )}. Você vai economizar R$${economia.toFixed(2)}.`;
 }
